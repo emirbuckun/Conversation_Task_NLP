88700c73f48b13febfec7bb65f3c66ff55865ba8
MustafaOzgur59 <101007632+MustafaOzgur59@users.noreply.github.com>
2024-04-23 21:48:38 +0300

    Update transformToAlpaca.py

--- delivery2/scripts/transformToAlpaca.py
+++ delivery2/scripts/transformToAlpaca.py
@@ -3,13 +3,9 @@ import json
 import deepl
 
 auth_key = ""  # Replace with your key
-
 translator = deepl.Translator(auth_key)
 
-result = translator.translate_text("Hello, world!", target_lang="TR")
-print(result.text)
 file = open("") # Replace with your file path
-print(type(file))
 
 csvreader = csv.reader(file)
 header = []
@@ -27,5 +23,4 @@ with open("alpacaFormat.json", "w", encoding="utf-8") as file:
             "output": output_TR
         }
         rows.append(new_row)
-        print(new_row)
     json.dump(rows, file, indent=2, ensure_ascii=False)
